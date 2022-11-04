0d4b77473a994ca128d560079b8cc501034384e0
Nikita Sychev <nikitasychev@inbox.ru>
2022-11-04 15:17:51 +0500

    Task 1 done, task 2 done ( но без определения существования файла )

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,14 @@
 #include <string>
 
 
+bool overwrite(std::string path, std::string str);
+
+
+
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int m, n;
-	std::string tmp;
+	
+	std::string tmp, tmp1;
 
 	std::cout << "Task 1: \n";
 	std::string path = "file.txt";
@@ -23,6 +27,7 @@ int main() {
 	}
 	else
 		std::cout << "Ошибка открытия файла: \n";
+	out.close();
 
 	std::ifstream in; 
 	in.open("file.txt");
@@ -41,27 +46,30 @@ int main() {
 
 
 	std::cout << "Task 2: \n";
+	std::string path1 = "file1.txt";
+	std::cout << "Введите строку: \n";
+	std::getline(std::cin, tmp1);
 
-
-
-
-
-
-
-
-
-	out.close();
-
-
-
-
-
-
-
-
-
+	if (overwrite(path1, tmp1))
+		std::cout << "TRUE\n";
+	else
+		std::cout << "FALSE\n";
 
 
 
 	return 0;
+}
+
+bool overwrite(std::string path, std::string str) {
+	std::ofstream out;
+	out.open(path, std::ios::out);
+	if (out.is_open()) {
+		out << str + '\n';
+		out.close();
+		return true;
+	}
+	else
+		return false;
+
+
 }
